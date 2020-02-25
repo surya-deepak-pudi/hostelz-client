a7ea04cd72c36099b3d107529c5d3442e62e3c04
surya-deepak-pudi <surya2deepak@gmail.com>
2020-02-26 02:18:25 +0530

    Updates

---
+++ precache-manifest.7fd08afa5c82da3e65798a2973fac7df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b95f31a84c6bb12a660f6b807de186b",
+    "url": "/hostelz-cient/index.html"
+  },
+  {
+    "revision": "617ef630f7b073243a1d",
+    "url": "/hostelz-cient/static/js/2.068b31c3.chunk.js"
+  },
+  {
+    "revision": "64d0d3c477447f9ff18d",
+    "url": "/hostelz-cient/static/js/main.a5b5db28.chunk.js"
+  },
+  {
+    "revision": "0f317cdc2e622a1a957c",
+    "url": "/hostelz-cient/static/js/runtime-main.3137aea2.js"
+  },
+  {
+    "revision": "8ba65c1e24ea7bd4b50e0f69a4bef3f9",
+    "url": "/hostelz-cient/static/media/nodata.8ba65c1e.png"
+  }
+]);
