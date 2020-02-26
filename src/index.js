2ee6b59fcff93ceee7f58baae7974c151735b559
surya-deepak-pudi <surya2deepak@gmail.com>
2020-02-26 06:41:48 +0530

    fixed env variable

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ if (localStorage.jwtToken) {
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter basename="process.env.PUBLIC_URL">
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
     </HashRouter>
   </Provider>,
