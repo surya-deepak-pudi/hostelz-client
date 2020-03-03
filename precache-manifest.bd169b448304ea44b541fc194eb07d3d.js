a93b2c5cd301e820441f717843ab790235caf840
surya-deepak-pudi <surya2deepak@gmail.com>
2020-03-03 12:10:13 +0530

    Updates

---
+++ precache-manifest.bd169b448304ea44b541fc194eb07d3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4390ec3d9f318360bcad879dcf015273",
+    "url": "/hostelz-client/index.html"
+  },
+  {
+    "revision": "990512e52ea0fbb81c5d",
+    "url": "/hostelz-client/static/js/2.028eb648.chunk.js"
+  },
+  {
+    "revision": "28a24813cb7cc3a7f5d4",
+    "url": "/hostelz-client/static/js/main.75722f84.chunk.js"
+  },
+  {
+    "revision": "b4cbcaefb7fe1ab6e3f2",
+    "url": "/hostelz-client/static/js/runtime-main.62774707.js"
+  },
+  {
+    "revision": "7bcadaa35d71876a40d3a1f2051512fe",
+    "url": "/hostelz-client/static/media/maintainence.7bcadaa3.png"
+  },
+  {
+    "revision": "8ba65c1e24ea7bd4b50e0f69a4bef3f9",
+    "url": "/hostelz-client/static/media/nodata.8ba65c1e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hostelz-client/precache-manifest.cd687228300547cb6c929f712219f1a3.js"
+  "/hostelz-client/precache-manifest.bd169b448304ea44b541fc194eb07d3d.js"
 );
 
 self.addEventListener('message', (event) => {
